633bb0a5ae121877541d89d786877697d7c8789d
a-Daley <april@mf-lt-april.local>
2016-12-22 21:17:07 -0500

    more changes

--- js/project.js
+++ js/project.js
@@ -6,15 +6,18 @@ $("button#search").on("click", function(){
 //	console.log(restaurantName);
 	var restaurantZip = $("#search-bar-zipcode").val();
 //	console.log(restaurantZip);
+// this pulls data from the API
 	var resultsData = $.getJSON("https://data.cityofnewyork.us/resource/9w7m-hzhe.json?dba=" + restaurantName.toUpperCase() + "&zipcode=" + restaurantZip, function(data) {
-		data.forEach(function(gradingEvent) {
+//		data.forEach(function(gradingEvent) {
 //		console.log(gradingEvent["action"]); 
-	})
+//		});
+
 		if (_.size(resultsData) == 0) {
 			alert("No restaurant found. Try again!");
 		}
 		var onlyWithGrades = data.filter(function(gradingEvent) {
-				return gradingEvent["grade"];})
+			return gradingEvent["grade"];
+		})
 //		console.log(onlyWithGrades);
 		if (_.size(onlyWithGrades) == 0) {
 			alert("No restaurant found. Try again!");
@@ -37,25 +40,23 @@ $("button#search").on("click", function(){
 		$("p#results-grade").text(restaurantGrade);
 
 		var findGradeDescrip = function(restaurantGrade) {
-	// var restaurantInput = $("#search-bar").val();
-	// var restaurantName = restaurantInput.toUpperCase()
-	// var restaurantZip = $("#search-bar-zipcode").val();
-	$.getJSON("https://data.cityofnewyork.us/resource/9w7m-hzhe.json?dba=" + restaurantName.toUpperCase() + "&zipcode=" + restaurantZip , function(data) {
-			var restaurantGrade = (lastEntry["grade"]);
-			if (restaurantGrade === "A") {
-				var newGradeDescrip = $("p#a-grade-descrip").text();
-				$("p#result-grade-descrip").text(newGradeDescrip);
-			} else if (restaurantGrade === "B") {
-				var newGradeDescrip = $("p#b-grade-descrip").text();
-				$("p#result-grade-descrip").text(newGradeDescrip);} else {
-					var newGrade = $("p#c-grade").val();
-					var newGradeDescrip = $("p#c-grade-description").val();
-					$("p#restaurantGrade").text(newGrade);
-					$("p#restauant-grade-descrip").text(newGradeDescrip);
+			$.getJSON("https://data.cityofnewyork.us/resource/9w7m-hzhe.json?dba=" + restaurantName.toUpperCase() + "&zipcode=" + restaurantZip , function(data) {
+				var restaurantGrade = (lastEntry["grade"]);
+				if (restaurantGrade === "A") {
+					var newGradeDescrip = $("p#a-grade-descrip").text();
+					$("p#result-grade-descrip").text(newGradeDescrip);
+				} else if (restaurantGrade === "B") {
+					var newGradeDescrip = $("p#b-grade-descrip").text();
+					$("p#result-grade-descrip").text(newGradeDescrip);
+				} else {
+						var newGrade = $("p#c-grade").val();
+						var newGradeDescrip = $("p#c-grade-description").val();
+						$("p#restaurantGrade").text(newGrade);
+						$("p#restauant-grade-descrip").text(newGradeDescrip);
 
- 		}
-	})
- }
+				}
+		})
+		}
 
 		findGradeDescrip(restaurantGrade);
 		$("p#result-grade-descrip").text
